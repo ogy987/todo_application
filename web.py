a430cca4c5b4a8cb59ae6e824b07995224765695
Ognjen Gvozdenovic <end81060@adobe.com>
2025-12-02 17:40:15 +0100

    Implement "Add new todo item" feature

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
 st.title("Todo App")
 st.subheader("This is todo app.")
 st.write("This app is to increase your productivity.")
@@ -10,8 +16,11 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="New todo:", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 print("test")
+
+st.session_state
 
 # streamlit run web.py --server.headless true
